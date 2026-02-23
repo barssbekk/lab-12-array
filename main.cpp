8b9ac3819a5589eef6c2d7e96409e22531b66df2
Barsbek Azamatov <bars.moderside@gmail.com>
2026-02-22 20:05:31 -0800

    Declare arr

--- main.cpp
+++ main.cpp
@@ -6,10 +6,13 @@
 using namespace std;
 
 int main() {
+    const int maxAmount{50};
     ifstream fileInput{"data.txt"};
     if (!fileInput) {
         cerr << "File not found";
         return 1;
     }
+
+    array<int, maxAmount> leaderboard{};
     return 0;
 }
